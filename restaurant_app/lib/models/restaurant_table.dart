b56c1093177548d84775f1bb6e7126b7fbd40f28
YandryRo97 <yandry.yr99@gmail.com>
2025-10-04 10:18:32 -0500

    Mesas

--- restaurant_app/lib/models/restaurant_table.dart
+++ restaurant_app/lib/models/restaurant_table.dart
@@ -3,9 +3,10 @@ final String id;
 final int number;
 final String status; // free|occupied|reserved
 final String? currentOrderId;
+final int? seats;
 
 
-RestaurantTable({required this.id, required this.number, required this.status, this.currentOrderId});
+RestaurantTable({required this.id, required this.number, required this.status, this.currentOrderId, this.seats});
 
 
 factory RestaurantTable.fromMap(String id, Map<String, dynamic> data) => RestaurantTable(
@@ -13,6 +14,7 @@ id: id,
 number: data['number'] ?? 0,
 status: data['status'] ?? 'free',
 currentOrderId: data['currentOrderId'],
+seats: data['seats'] ?? 0
 );
 
 
@@ -20,5 +22,6 @@ Map<String, dynamic> toMap() => {
 'number': number,
 'status': status,
 'currentOrderId': currentOrderId,
+'seats': seats,
 };
 }
